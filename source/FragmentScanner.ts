a58e82e965615c133cea1f5c6b1f6375204bda71
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-01-22 23:22:41 +0100

    fix: Name generation not respected

--- source/FragmentScanner.ts
+++ source/FragmentScanner.ts
@@ -13,7 +13,9 @@ export class FragmentScanner {
 
   async loadFragments() {
     const fragmentStore = new FragmentStore(this.fragmentDirectory);
-    const fragments = await globby(`.*.md`, { cwd: this.fragmentDirectory });
+    const fragments = await globby(FragmentStore.scriptToFragmentFilename("*"), {
+      cwd: this.fragmentDirectory,
+    });
     for (const fragment of fragments) {
       await fragmentStore.loadFragment(fragment);
     }
