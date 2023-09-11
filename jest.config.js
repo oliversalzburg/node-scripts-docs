df3f6e5dc1d2e34d5e4b19af4cef9106302796b8
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-09-11 11:24:10 +0200

    test: Update Jest snapshots/config

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
   coverageDirectory: "coverage",
   coverageReporters: ["html-spa", "lcovonly", "text-summary"],
   collectCoverageFrom: ["source/**/*.ts"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-    },
-  },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    "^.+\\.tsx?$": ["ts-jest"],
+  },
 };
